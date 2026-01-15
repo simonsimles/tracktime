41a76e57026683d348bc696ac31afd674afab127
simonsimles <simonsimles@simles.de>
2026-01-15 19:02:01 +0100

    Update getWeekFromDate to compute weeks correctly fpr 2026

--- ui/src/Date.tsx
+++ ui/src/Date.tsx
@@ -1,7 +1,8 @@
 export const getWeekFromDate = function(d: Date) {
     const endDate: number = Number(new Date(d.getFullYear(), d.getMonth(), d.getDate() + 4 - (d.getDay() || 7)))
     const startDate: number = Number(new Date(d.getUTCFullYear(), 0, 1))
-    return Math.ceil(((endDate - startDate) / (DAY_IN_SECONDS)) / 7)
+    const days = Math.floor((endDate - startDate) / DAY_IN_SECONDS) + 1;
+    return Math.ceil(days / 7);
 }
 
 export const DAY_IN_SECONDS = 24 * 60 *60 *1000
